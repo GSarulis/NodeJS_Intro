dfc2e5dba8a241df5e5b2dd08e65478509946acc
GSarulis <g.v.sarulis@gmail.com>
2022-08-01 17:53:25 +0300

    learning node js

---
+++ index.js
@@ -0,0 +1,7 @@
+const casual = require("casual");
+
+console.log(casual.city, casual.integer(1, 10));
+
+// random skaiciaus funkcija kurioje galime irasyti nuo kiek iki kiek skaicius bus ir ji returninti
+
+console.log(casual.name_prefix, casual.full_name);
