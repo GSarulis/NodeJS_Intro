f27d0f007b21c7a50aeda4e3867cd17b27687fed
GSarulis <g.v.sarulis@gmail.com>
2022-08-02 18:34:06 +0300

    learning node js

---
+++ 3paskaita/client/script.js
@@ -0,0 +1,19 @@
+console.log("Laba diena");
+
+fetch("http://localhost:8080/")
+  .then((resp) => resp.json())
+  .then((response) => {
+    const ul = document.createElement("ul");
+
+    response.forEach((car) => {
+      const li = document.createElement("li");
+      ``;
+      li.textContent = car;
+
+      ul.append(li);
+    });
+
+    document.body.append(ul);
+    nv;
+  })
+  .catch((error) => console.error(error));
--- 3paskaita/index.js
+++ 3paskaita/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+const PORT = 8080;
+
+// "/" - base path ( pagrindinis kelias) pvz.: localhost:3000/
+// req - sutrumpinimas request zodzio. Kvietimas is vartotojo puses
+// res - sutrumpinimas response zodzio. Grazinimas is serverines dalies
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(["BMW", "VW", "AUDI", "BENTLEY"]);
 });
 
-app.listen(3000);
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
