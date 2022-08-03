3f3620b3a3b713eced7983de6466650fe9689198
GSarulis <g.v.sarulis@gmail.com>
2022-08-03 20:14:25 +0300

    running app NODE JS

---
+++ 5pamoka/3.2/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+const PORT = 8080;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const names = [{ name: "Gediminas", surname: "Sarulis" }];
+
+app.get("/names", (req, res) => {
+  res.status = 200;
+  res.send(names);
+});
+
+app.post("/names", (req, res) => {
+  console.log(req.body);
+
+  names.push(req.body);
+  res.send(req.body);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
